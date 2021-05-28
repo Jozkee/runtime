88ab48de60fcf3e250f592adabe12c5ac2aa2625
David Cantu <dacantu@microsoft.com>
2021-05-28 14:38:13 -0700

    Progress so far on LinkTarget

--- src/libraries/System.Private.CoreLib/src/System/IO/FileSystemInfo.cs
+++ src/libraries/System.Private.CoreLib/src/System/IO/FileSystemInfo.cs
@@ -4,6 +4,7 @@
 using System;
 using System.IO;
 using System.Runtime.Serialization;
+using Microsoft.Win32.SafeHandles;
 
 #if MS_IO_REDIST
 namespace Microsoft.IO
@@ -105,6 +106,47 @@ namespace System.IO
         {
             get => LastWriteTimeCore.UtcDateTime;
             set => LastWriteTimeCore = File.GetUtcDateTimeOffset(value);
+        }
+
+        public string? LinkTarget => _linkTarget ??= RefreshLinkTarget();
+        private string? _linkTarget;
+
+        private string? RefreshLinkTarget()
+        {
+            // Catch all non-links here.
+            if ((Attributes & FileAttributes.ReparsePoint) == 0)
+            {
+                return null;
+            }
+
+            // Get the raw link target.
+
+            return null!;
+        }
+
+        // DeviceIoControl in Windows.
+        // readlink in Unix.
+        private unsafe string? GetLinkTargetCore()
+        {
+            // this needs to be moved to .Windows.cs and the parity method needs to be added on .Unix.cs
+            SafeFileHandle handle = Interop.Kernel32.CreateFile(
+                FullName,
+                dwDesiredAccess: 0,
+                FileShare.ReadWrite | FileShare.Delete,
+                lpSecurityAttributes: (Interop.Kernel32.SECURITY_ATTRIBUTES*) IntPtr.Zero,
+                FileMode.Open,
+                dwFlagsAndAttributes: Interop.Kernel32.FileOperations.FILE_FLAG_BACKUP_SEMANTICS | Interop.Kernel32.FileOperations.FILE_FLAG_OPEN_REPARSE_POINT,
+                hTemplateFile: IntPtr.Zero);
+
+
+            // TODO: Need to find a way to pass lpOutBuffer and then cast it to REPARSE_DATA_BUFFER so we can extract the link target path from it.
+
+            //Interop.Kernel32.DeviceIoControl(
+            //    handle,
+            //    Interop.Kernel32.FSCTL_GET_REPARSE_POINT,
+            //    lpInBuffer: IntPtr.Zero, nInBufferSize: 0,
+            //    lpOutBuffer: )
+            return null!;
         }
 
         /// <summary>
