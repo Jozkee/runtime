f9d2f9cd29ed50189ea946ec2a67dca5dc6d2e54
David Cantu <dacantu@microsoft.com>
2023-03-22 19:01:36 -0500

    Add project skeleton

---
+++ src/libraries/System.IO.StreamFactory/tests/StreamFactoryTests.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Xunit;
+
+namespace System.IO.StreamFactory.Tests
+{
+    public class StreamFactoryTests
+    {
+        [Fact]
+        public void Qt()
+        {
+            Console.WriteLine("Test running...");
+        }
+    }
+}
